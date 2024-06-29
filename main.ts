ca83516f9518601ad66647a3db24278c16d1c269
fiskardog <fiskar@verizon.net>
2024-06-29 08:19:09 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Happy)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+keyboard.startKeyboardService()
+basic.forever(function () {
+	
+})

54e856aca2bcdf216c9ee42ee22207f3b824ac61
fiskardog <fiskar@verizon.net>
2024-06-30 17:14:50 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,21 @@
 bluetooth.onBluetoothConnected(function () {
     basic.showIcon(IconNames.Happy)
+    connState = 1
 })
 bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.Sad)
+    connState = 0
 })
 input.onButtonPressed(Button.A, function () {
     keyboard.sendString(" ")
     basic.showString(" ")
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    if (connState == 1) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
 })
 input.onButtonPressed(Button.B, function () {
     keyboard.sendString(".")
@@ -19,6 +25,7 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     keyboard.sendString("Z")
     basic.showString("Z")
 })
+let connState = 0
 basic.showIcon(IconNames.Heart)
 keyboard.startKeyboardService()
 basic.forever(function () {

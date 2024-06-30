7b46feee8a966edbe60bdbee1a79b0f2e227d4f7
fiskardog <fiskar@verizon.net>
2024-06-30 11:32:24 -0400

    Customize for Sqview

--- main.ts
+++ main.ts
@@ -5,15 +5,15 @@ bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.Sad)
 })
 input.onButtonPressed(Button.A, function () {
-    keyboard.sendString("A")
-    basic.showString("A")
+    keyboard.sendString(" ")
+    basic.showString(" ")
 })
 input.onButtonPressed(Button.AB, function () {
 	
 })
 input.onButtonPressed(Button.B, function () {
-    keyboard.sendString("B")
-    basic.showString("B")
+    keyboard.sendString(".")
+    basic.showString(".")
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     keyboard.sendString("Z")

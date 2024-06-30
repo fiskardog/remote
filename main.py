e4c2b0e078fa69c2f7c3f67593d1e9978e4d143c
fiskardog <fiskar@verizon.net>
2024-06-30 11:30:45 -0400

    Add characters to xmit

---
+++ main.py
@@ -0,0 +1,29 @@
+def on_bluetooth_connected():
+    basic.show_icon(IconNames.HAPPY)
+bluetooth.on_bluetooth_connected(on_bluetooth_connected)
+
+def on_bluetooth_disconnected():
+    basic.show_icon(IconNames.SAD)
+bluetooth.on_bluetooth_disconnected(on_bluetooth_disconnected)
+
+def on_button_pressed_a():
+    pass
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    pass
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    pass
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_logo_pressed():
+    pass
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+keyboard.start_keyboard_service()
+
+def on_forever():
+    pass
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -5,17 +5,21 @@ bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.Sad)
 })
 input.onButtonPressed(Button.A, function () {
-	
+    keyboard.sendString("A")
+    basic.showString("A")
 })
 input.onButtonPressed(Button.AB, function () {
 	
 })
 input.onButtonPressed(Button.B, function () {
-	
+    keyboard.sendString("B")
+    basic.showString("B")
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-	
+    keyboard.sendString("Z")
+    basic.showString("Z")
 })
+basic.showIcon(IconNames.Heart)
 keyboard.startKeyboardService()
 basic.forever(function () {
 	
